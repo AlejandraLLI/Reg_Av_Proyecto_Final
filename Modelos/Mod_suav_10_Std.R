919e2a1c25ac336860821fc9682a03c516c15686
AlejandraLLI <alelli3@hotmail.com>
2018-12-03 09:46:39 -0600

    Agregando archivos proyecto final con cambios Lau

--- Modelos/Mod_suav_10_Std.R
+++ Modelos/Mod_suav_10_Std.R
@@ -48,3 +48,6 @@ out_dinam_std_suav_10.yp<-out_dinam_std_suav_10.sum[grep("yp",rownames(out_dinam
 
 #-Betas-
 out_dinam_std_suav_10.beta<-out_dinam_std_suav_10.sum[grep("beta",rownames(out_dinam_std_suav_10.sum)),]
+
+# -pseudoR2-
+pseudoR2_dinam_std_suav_10<-pseudoR2(out.yp=out_dinam_std_suav_10.yp)
